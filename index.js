466c3bdb0c40ee69bab847ba750ab8c4e7619430
lucas-bui <luan237@gmail.com>
2022-06-07 16:27:33 -0400

    GET method done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8080;
-const videosData = require("./data/videos.json");
+const videos = require("./routes/videos");
 
 app.use(express.json());
-app.use(express.cors());
+app.use(cors());
+app.use("/", videos);
+app.use(express.static("./public"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const videosData = require("./data/videos.json");
+const videosData = require("../data/videos.json");
+
+router.route("/videos").get((req, res) => {
+  const basicData = [];
+  videosData.forEach((video) => {
+    const { id, title, channel, image } = video;
+    basicData.push({ id, title, channel, image });
+  });
+  res.send(basicData);
+});
+
+router.route("/videos/:id").get((req, res) => {
+  const requestedId = req.params.id;
+  const foundVideo = videosData.find((video) => video.id === requestedId);
+  res.send(foundVideo);
+  console.log(requestedId);
+});
 
 module.exports = router;
