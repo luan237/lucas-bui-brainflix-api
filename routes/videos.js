4022f116c8761f0899ff8c1a1081e2745102f004
lucas-bui <luan237@gmail.com>
2022-06-07 17:36:01 -0400

    working on video posting

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const videosData = require("../data/videos.json");
+let videosData = require("../data/videos.json");
+const uuid = require("uuid");
+const fs = require("fs");
 
 router.route("/videos").get((req, res) => {
   const basicData = [];
@@ -15,7 +17,27 @@ router.route("/videos/:id").get((req, res) => {
   const requestedId = req.params.id;
   const foundVideo = videosData.find((video) => video.id === requestedId);
   res.send(foundVideo);
-  console.log(requestedId);
+});
+
+router.route("/videos").post((req, res) => {
+  const newVideo = {
+    id: uuid.v4(),
+    title: req.body.title,
+    channel: "New User",
+    image: "http://localhost:8080/Upload-video-preview.jpg",
+    description: req.body.description,
+    views: 10000,
+    likes: 10,
+    duration: "5:01",
+    video: "https://www.youtube.com/watch?v=fpoBdxnvlcU",
+    timestamp: new Date(),
+    comments: [],
+  };
+  videosData.push(newVideo);
+  fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
+    console.log(err);
+  });
+  res.send(newVideo);
 });
 
 module.exports = router;
