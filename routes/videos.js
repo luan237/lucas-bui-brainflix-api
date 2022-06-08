4ee1c3ed57a3bb2f36cce00634f1660f41ed13bc
lucas-bui <luan237@gmail.com>
2022-06-08 15:47:33 -0400

    post and delete, like comment function done

--- routes/videos.js
+++ routes/videos.js
@@ -3,25 +3,25 @@ const router = express.Router();
 let videosData = require("../data/videos.json");
 const uuid = require("uuid");
 const fs = require("fs");
-
+// get basic list of video
 router.route("/videos").get((req, res) => {
   const basicData = [];
   videosData.forEach((video) => {
     const { id, title, channel, image } = video;
     basicData.push({ id, title, channel, image });
   });
-  res.send(basicData);
+  res.status(200).json(basicData);
 });
 
+// get a specific video details
 router.route("/videos/:id").get((req, res) => {
   const requestedId = req.params.id;
   const foundVideo = videosData.find((video) => video.id === requestedId);
-  res.send(foundVideo);
+  res.status(200).json(foundVideo);
 });
 
+// post new Video
 router.route("/videos").post((req, res) => {
-  const requestBody = JSON.stringify(req.body);
-  console.log(requestBody);
   const newVideo = {
     id: uuid.v4(),
     title: req.body.title,
@@ -39,7 +39,51 @@ router.route("/videos").post((req, res) => {
   fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
     console.log(err);
   });
-  res.send(newVideo);
+  res.status(200).json(newVideo);
 });
+
+// diving deeper (post comment)
+router.post("/videos/:id/comments", (req, res) => {
+  const { name, comment } = req.body;
+  const foundVideo = videosData.find((video) => video.id === req.params.id);
+  foundVideo.comments.push({
+    name,
+    comment,
+    id: uuid.v4(),
+    likes: 0,
+    timestamp: new Date(),
+  });
+  fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
+    console.log(err);
+  });
+  res.status(200).json(foundVideo);
+});
+
+// diving deeper (delete comment)
+router
+  .route("/videos/:id/comments/:commentId")
+  .delete((req, res) => {
+    const foundVideo = videosData.find((video) => video.id === req.params.id);
+    const foundComment = foundVideo.comments.find(
+      (comment) => comment.id === req.params.commentId
+    );
+    commentId = foundVideo.comments.indexOf(foundComment);
+    foundVideo.comments.splice(commentId, 1);
+    fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
+      console.log(err);
+    });
+    res.status(200).json(foundVideo);
+  })
+  .put((req, res) => {
+    const foundVideo = videosData.find((video) => video.id === req.params.id);
+    const foundComment = foundVideo.comments.find(
+      (comment) => comment.id === req.params.commentId
+    );
+    foundComment.likes += 1;
+    fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
+      console.log(err);
+    });
+    res.status(200).json(foundVideo);
+  });
 
 module.exports = router;
