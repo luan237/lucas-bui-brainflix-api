b393565d6937661450cc97d8404a08421ed165ff
Lucas <luan237@gmail.com>
2022-06-09 01:16:10 -0400

    image upload completed

--- routes/videos.js
+++ routes/videos.js
@@ -3,6 +3,8 @@ const router = express.Router();
 let videosData = require("../data/videos.json");
 const uuid = require("uuid");
 const fs = require("fs");
+const multer = require("multer");
+
 // get basic list of video
 router.route("/videos").get((req, res) => {
   const basicData = [];
@@ -20,29 +22,51 @@ router.route("/videos/:id").get((req, res) => {
   res.status(200).json(foundVideo);
 });
 
-// post new Video
-router.route("/videos").post((req, res) => {
-  console.log(req);
-  const newVideo = {
-    id: uuid.v4(),
-    title: req.body.title,
-    channel: "New User",
-    image: req.body.image,
-    description: req.body.description,
-    views: 10000,
-    likes: 10,
-    duration: "5:01",
-    video:
-      "https://project-2-api.herokuapp.com/stream/?api_key=5821f58b-d638-4339-9f50-71d86650f340",
-    timestamp: new Date(),
-    comments: [],
-  };
-  videosData.push(newVideo);
-  // fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
-  //   console.log(err);
-  // });
-  res.status(200).json(newVideo);
+// set destination and file name for image upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log("Hello World");
+    cb(null, "./public/");
+  },
+  filename: function (req, file, cb) {
+    console.log("Hello World 2");
+    cb(null, file.originalname);
+  },
 });
+
+// post new Video (with image or default image)
+router
+  .route("/videos")
+  .post(multer({ storage: storage }).single("thumbnail"), async (req, res) => {
+    let imageName;
+    if (req.file) {
+      imageName = req.file.originalname;
+    } else {
+      imageName = "Upload-video-preview.jpg";
+    }
+    const newVideo = {
+      id: uuid.v4(),
+      title: req.body.title,
+      channel: "New User",
+      image: `http://localhost:8080/${imageName}`,
+      description: req.body.description,
+      views: 10000,
+      likes: 10,
+      duration: "5:01",
+      video:
+        "https://project-2-api.herokuapp.com/stream/?api_key=5821f58b-d638-4339-9f50-71d86650f340",
+      timestamp: Date.now(),
+      comments: [],
+    };
+    videosData.push(newVideo);
+    fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Upload successfuly");
+      }
+    });
+    res.status(200).send(newVideo);
+  });
 
 // diving deeper (post comment)
 router.post("/videos/:id/comments", (req, res) => {
@@ -53,7 +77,7 @@ router.post("/videos/:id/comments", (req, res) => {
     comment,
     id: uuid.v4(),
     likes: 0,
-    timestamp: new Date(),
+    timestamp: Date.now(),
   });
   fs.writeFile("./data/videos.json", JSON.stringify(videosData), (err) => {
     console.log(err);
@@ -76,6 +100,8 @@ router
     });
     res.status(200).json(foundVideo);
   })
+
+  // diving deeper (like comment)
   .put((req, res) => {
     const foundVideo = videosData.find((video) => video.id === req.params.id);
     const foundComment = foundVideo.comments.find(
